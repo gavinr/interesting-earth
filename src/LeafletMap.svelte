29f8095cdd5a828246ab9298cf5d945854103950
Gavin Rehkemper <gavin@gavinr.com>
2020-10-14 23:19:28 -0500

    cleanup

--- src/LeafletMap.svelte
+++ src/LeafletMap.svelte
@@ -11,11 +11,8 @@
 
   onMount(() => {
     var map = L.map(mapDomNode).setView([center[1], center[0]], zoom);
-
-    // L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    //   attribution:
-    //     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    // }).addTo(map);
+    map.scrollWheelZoom.disable();
+    map.attributionControl.setPrefix(false);
 
     L.tileLayer(
       "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
